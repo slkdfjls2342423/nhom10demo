b2216f2ca5fd13f2827dc19c63efed0dc435bb42
LeNgocPhong <lengocphongk4@gmail.com>
2025-10-27 11:26:53 +0700

    Hieu

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,15 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int hieu(int a,int b){
+	return a+b;
+}
 int main()
 {
+	int a,b;
+	cin>>a>>b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout<<"Hieu= "<<hieu(a,b)<<endl;
 	system("pause");
 	return 0;
 }
