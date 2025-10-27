002ecd8bd6f149c40d23c505fb7c3debcfe49c73
Dang Manh Tan <dangmanhtan0@gmail.com>
2025-10-27 11:24:53 +0700

    add cong function

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,17 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+
+int tong (int a,int b){
+    return a+b;
+}
 int main()
 {
+    int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+    cout<<"nhap vao a: "; cin>>a;
+    cout<<"nhap vao b: "; cin>>b;
+    cout<<"tong cua a va b la: "<<tong(a,b)<<endl;
 	system("pause");
 	return 0;
 }
